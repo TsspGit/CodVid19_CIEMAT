eb5b237a2c96712e370a09c2e7f83b047b48bb12
Tomas Sanchez <tssanchezpastor@gmail.com>
2020-04-22 19:28:08 +0200

    new branch

--- codvidutils/nwpic.py
+++ codvidutils/nwpic.py
@@ -24,3 +24,25 @@ def new_pictures_arrays(pictures, strides=5, kernel=(180,180)):
     news= np.array(news)
     
     return news.reshape(news.shape[0]*news.shape[1], kernel[0],kernel[1],pictures.shape[3])
+
+def underbalance_imgs(diseaseID, X):
+    '''
+    This function balances the data:
+    Inputs: disease_ID: array of the train and test categories.
+            X: array of the pics.
+    Usage:
+        -> disease_ID, X = balance_imgs(disease_ID, X)
+    '''
+    from collections import Counter
+    from imblearn.under_sampling import RandomUnderSampler
+    counter = Counter (diseaseID)
+    print('Count of classes: ', counter)
+    dicto = {2: 4500, 0: 4500, 1:97}
+    X = X.reshape(X.shape[0],-1)
+    under = RandomUnderSampler(sampling_strategy =dicto)
+    X, diseaseID = under.fit_resample(X, diseaseID)
+    # summarize class distribution
+    print('New diseaseID shape: ', diseaseID.shape)
+    print('New X shape: ', X.shape)
+    print('New Count of classes: ', Counter (diseaseID))
+    return diseaseID, X
