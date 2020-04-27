a05f779cbf339790ead0fe335c715169abb16faf
Tomas Sanchez <tssanchezpastor@gmail.com>
2020-04-27 14:35:08 +0200

    new changes

--- Random_Forest_Regressor.py
+++ Random_Forest_Regressor.py
@@ -18,8 +18,8 @@ from codvidutils.cudasession import set_session
 from codvidutils import nwpic as nw
 
 set_session()
-train_class = pd.read_csv('data/train_split_v3.csv', sep=' ', header=1, names=['patientID', 'image_path', 'class'])
-test_class = pd.read_csv('data/test_split_v3.csv', sep=' ', header=1, names=['patientID', 'image_path', 'class'])
+train_class = pd.read_csv('data/train_split_v4.csv', sep=' ', header=1, names=['patientID', 'image_path', 'class'])
+test_class = pd.read_csv('data/test_split_v4.csv', sep=' ', header=1, names=['patientID', 'image_path', 'class'])
 values_dict = {'pneumonia': 2, 'COVID-19': 1, 'normal': 0}
 test_class['class_categorical'] = test_class['class'].apply(map_categorical, args=(values_dict,))
 train_class['class_categorical'] = train_class['class'].apply(map_categorical, args=(values_dict,))
@@ -97,7 +97,7 @@ Y_test[Y_test==2]=0
 
 X_test = X_test/255
 X_train = X_train/255
-best_model_path = 'Autoencoder_covid.hdf5'
+best_model_path = 'Autoencoder_covid_v4.hdf5'
 model = load_model(best_model_path)
 encoder = Model(model.layers[0].input, model.layers[6].output)
 encoder_imgs = encoder.predict(X_test)
@@ -109,7 +109,7 @@ encoder_RF_train = encoder.predict(X_train)
 print('encoder_RF_train.shape', encoder_RF_train.shape)
 encoder_RF_train = encoder_RF_train.reshape((encoder_RF_train.shape[0], 23*23*128))
 # Regressor:
-RF = RandomForestRegressor(n_estimators=100, n_jobs=-1)
+RF = RandomForestRegressor(n_estimators=200, n_jobs=-1)
 RF.fit(encoder_RF_train, Y_train)
 encoder_imgs = encoder_imgs.reshape((encoder_imgs.shape[0], 23*23*128))
 preds = RF.predict(encoder_imgs)
@@ -117,58 +117,4 @@ nocovid = preds[np.where(Y_test == 0)]
 covid = preds[np.where(Y_test == 1)]
 print("\n\n---------- Predictions ----------\n")
 print("preds = ", preds)
-np.savetxt('preds_RFr.txt', preds, delimiter=',')
-# Counting by thresholds
-TP_050 = np.count_nonzero(np.where((Y_test==1) & (preds>0.50)))
-FN_050 = np.count_nonzero(np.where((Y_test==1) & (preds<0.50)))
-FP_050 = np.count_nonzero(np.where((Y_test==0) & (preds>0.50)))
-TN_050 = np.count_nonzero(np.where((Y_test==0) & (preds<0.50)))
-cm = np.array([[TP_050, TN_050],[FN_050, FP_050]])
-print('{} of {} no COVID-19'.format(cm[0,0], np.sum(Y_test == 0)))
-print('{} of {} COVID-19'.format(cm[1,1], np.sum(Y_test == 1)))
-cm = normalize(cm, 'l1')
-labels = ['no COVID-19', 'COVID-19']
-fig1, ax = plt.subplots(figsize=(8, 6))
-cax = ax.matshow(cm, cmap='YlGnBu', vmin=0, vmax=1)
-cbar = fig1.colorbar(cax)
-cbar.set_ticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
-cbar.set_ticklabels(['0', '0.2', '0.4', '0.6', '0.8', '1.0'])
-cbar.ax.set_ylabel('Accuracy', rotation=270, labelpad=20, fontsize=16)
-ax.set_xticklabels([''] + labels)
-ax.set_yticklabels([''] + labels)
-ax.set_ylabel('Expected', fontsize=16)
-ax.set_xlabel('Predicted', fontsize=16)
-fig1.savefig('Figures/cm_RFregressor.png', dpi=200)
-
-fig2, ax = plt.subplots(figsize=(8,6))
-ax.hist(nocovid, 50, histtype='step', lw=2, color=darkorange, label='No COVID')
-ax.hist(covid, 50, histtype='step', lw=2, color=darkorange, label='COVID')
-plt.grid()
-ax.set_ylabel('Number of events /0.01')
-plt.set_xlabel('Score')
-plt.legend( loc='upper center')
-plt.yscale("log")
-fig2.savefig('Figures/histogram_RFregressor.png', dpi=200)
-
-# Classifier:
-RFc = RandomForestClassifier(n_estimators=100, n_jobs=-1)
-RFc.fit(encoder_RF_train, Y_train)
-preds = RFc.predict(encoder_imgs)
-cm = confusion_matrix(Y_test, preds)
-print("Accuracy:", accuracy_score(Y_test, preds))
-print('{} of {} no COVID-19'.format(cm[0,0], np.sum(Y_test == 0)))
-print('{} of {} COVID-19'.format(cm[1,1], np.sum(Y_test == 1)))
-print(cm)
-cm = normalize(cm, 'l1')
-labels = ['no COVID-19', 'COVID-19']
-fig3, ax = plt.subplots(figsize=(8, 6))
-cax = ax.matshow(cm, cmap='YlGnBu', vmin=0, vmax=1)
-cbar = fig3.colorbar(cax)
-cbar.set_ticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
-cbar.set_ticklabels(['0', '0.2', '0.4', '0.6', '0.8', '1.0'])
-cbar.ax.set_ylabel('Accuracy', rotation=270, labelpad=20, fontsize=16)
-ax.set_xticklabels([''] + labels)
-ax.set_yticklabels([''] + labels)
-ax.set_ylabel('Expected', fontsize=16)
-ax.set_xlabel('Predicted', fontsize=16)
-fig3.savefig('Figures/cm_RFclassifier.png', dpi=200)
+np.savetxt('preds_RFr_v4.txt', preds, delimiter=',')
